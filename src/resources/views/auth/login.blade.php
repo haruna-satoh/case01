554e6dbc766d10f4d4b8a0b0bcd1a343ef67c2c4
haruna <news.sigeak@gmail.com>
2025-09-03 12:25:09 +0900

    ログインページのレイアウト

---
+++ src/resources/views/auth/login.blade.php
@@ -0,0 +1,46 @@
+<div class="login__flrama">
+    <div class="login__title">
+        <h2>ログイン</h2>
+    </div>
+    <form action="/login" method="post" class="form">
+        @csrf
+        <div class="form__group">
+            <div class="form__group--title">
+                <span>メールアドレス</span>
+            </div>
+            <div class="form__group--content">
+                <div class="form__group--input">
+                    <input type="email" name="email">
+                </div>
+                <div class="form__error">
+                    @error('email')
+                        {{ $message }}
+                    @enderror
+                </div>
+            </div>
+            <div class="form__group--title">
+                <span>パスワード</span>
+            </div>
+            <div class="form__group--content">
+                <div class="form__group--input">
+                    <input type="password" name="password">
+                </div>
+                <div class="form__error">
+                    @error('password')
+                        {{ $message }}
+                    @enderror
+                </div>
+            </div>
+        </div>
+        <div class="form__button">
+            <button class="form__button--submit" type="submit">
+                ログインする
+            </button>
+        </div>
+        <div class="form__button--register">
+            <a href="/register" class="form__register">
+                会員登録はこちら
+            </a>
+        </div>
+    </form>
+</div>
