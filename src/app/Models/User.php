5451e09fa701ad16ae3784dbcafb467f10005ffc
haruna <news.sigeak@gmail.com>
2025-09-12 20:31:05 +0900

    usersテーブルのDB保存設定

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -21,6 +21,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'icon',
+        'post_code',
+        'address',
+        'building',
     ];
 
     /**
