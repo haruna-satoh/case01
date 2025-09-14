195c3c2894fb95fdf9910173dbad9ad0a526bad1
haruna <news.sigeak@gmail.com>
2025-09-15 08:01:18 +0900

    商品一覧ページにヘッダーを継承

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,86 +1,90 @@
-<div class="flrama__flrama">
-    <div class="flrama__title">
-        <h3>おすすめ</h3>
-        <h3>マイリスト</h3>
+@extends('layouts.app')
+
+@section('content')
+    <div class="flrama__flrama">
+        <div class="flrama__title">
+            <h3>おすすめ</h3>
+            <h3>マイリスト</h3>
+        </div>
+        <div class="flrama__content">
+            <div class="flrama__content--img">
+                <img src="{{ asset('images/Armani+Mens+Clock.jpg') }}" alt="商品画像">
+            </div>
+            <div class="flrama__content--item">
+                <p>腕時計</p>
+            </div>
+        </div>
+        <div class="flrama__content">
+            <div class="flrama__content--img">
+                <img src="{{ asset('images/HDD+Hard+Disk.jpg') }}" alt="商品画像">
+            </div>
+            <div class="flrama__content--item">
+                <p>HDD</p>
+            </div>
+        </div>
+        <div class="flrama__content">
+            <div class="flrama__content--img">
+                <img src="{{ asset('images/iLoveIMG+d.jpg') }}" alt="商品画像">
+            </div>
+            <div class="flrama__content--item">
+                <p>玉ねぎ3束</p>
+            </div>
+        </div>
+        <div class="flrama__content">
+            <div class="flrama__content--img">
+                <img src="{{ asset('images/Leather+Shoes+Product+Photo.jpg') }}" alt="商品画像">
+            </div>
+            <div class="flrama__content--item">
+                <p>革靴</p>
+            </div>
+        </div>
+        <div class="flrama__content">
+            <div class="flrama__content--img">
+                <img src="{{ asset('images/Living+Room+Laptop.jpg') }}" alt="商品画像">
+            </div>
+            <div class="flrama__content--item">
+                <p>ノートPC</p>
+            </div>
+        </div>
+        <div class="flrama__content">
+            <div class="flrama__content--img">
+                <img src="{{ asset('images/Music+Mic+4632231.jpg') }}" alt="商品画像">
+            </div>
+            <div class="flrama__content--item">
+                <p>マイク</p>
+            </div>
+        </div>
+        <div class="flrama__content">
+            <div class="flrama__content--img">
+                <img src="{{ asset('images/Purse+fashion+pocket.jpg') }}" alt="商品画像">
+            </div>
+            <div class="flrama__content--item">
+                <p>ショルダーバッグ</p>
+            </div>
+        </div>
+        <div class="flrama__content">
+            <div class="flrama__content--img">
+                <img src="{{ asset('images/Tumbler+souvenir.jpg') }}" alt="商品画像">
+            </div>
+            <div class="flrama__content--item">
+                <p>タンブラー</p>
+            </div>
+        </div>
+        <div class="flrama__content">
+            <div class="flrama__content--img">
+                <img src="{{ asset('images/Waitress+with+Coffee+Grinder.jpg') }}" alt="商品画像">
+            </div>
+            <div class="flrama__content--item">
+                <p>コーヒーミル</p>
+            </div>
+        </div>
+        <div class="flrama__content">
+            <div class="flrama__content--img">
+                <img src="{{ asset('images/外出メイクアップセット.jpg') }}" alt="商品画像">
+            </div>
+            <div class="flrama__content--item">
+                <p>メイクセット</p>
+            </div>
+        </div>
     </div>
-    <div class="flrama__content">
-        <div class="flrama__content--img">
-            <img src="{{ asset('images/Armani+Mens+Clock.jpg') }}" alt="商品画像">
-        </div>
-        <div class="flrama__content--item">
-            <p>腕時計</p>
-        </div>
-    </div>
-    <div class="flrama__content">
-        <div class="flrama__content--img">
-            <img src="{{ asset('images/HDD+Hard+Disk.jpg') }}" alt="商品画像">
-        </div>
-        <div class="flrama__content--item">
-            <p>HDD</p>
-        </div>
-    </div>
-    <div class="flrama__content">
-        <div class="flrama__content--img">
-            <img src="{{ asset('images/iLoveIMG+d.jpg') }}" alt="商品画像">
-        </div>
-        <div class="flrama__content--item">
-            <p>玉ねぎ3束</p>
-        </div>
-    </div>
-    <div class="flrama__content">
-        <div class="flrama__content--img">
-            <img src="{{ asset('images/Leather+Shoes+Product+Photo.jpg') }}" alt="商品画像">
-        </div>
-        <div class="flrama__content--item">
-            <p>革靴</p>
-        </div>
-    </div>
-    <div class="flrama__content">
-        <div class="flrama__content--img">
-            <img src="{{ asset('images/Living+Room+Laptop.jpg') }}" alt="商品画像">
-        </div>
-        <div class="flrama__content--item">
-            <p>ノートPC</p>
-        </div>
-    </div>
-    <div class="flrama__content">
-        <div class="flrama__content--img">
-            <img src="{{ asset('images/Music+Mic+4632231.jpg') }}" alt="商品画像">
-        </div>
-        <div class="flrama__content--item">
-            <p>マイク</p>
-        </div>
-    </div>
-    <div class="flrama__content">
-        <div class="flrama__content--img">
-            <img src="{{ asset('images/Purse+fashion+pocket.jpg') }}" alt="商品画像">
-        </div>
-        <div class="flrama__content--item">
-            <p>ショルダーバッグ</p>
-        </div>
-    </div>
-    <div class="flrama__content">
-        <div class="flrama__content--img">
-            <img src="{{ asset('images/Tumbler+souvenir.jpg') }}" alt="商品画像">
-        </div>
-        <div class="flrama__content--item">
-            <p>タンブラー</p>
-        </div>
-    </div>
-    <div class="flrama__content">
-        <div class="flrama__content--img">
-            <img src="{{ asset('images/Waitress+with+Coffee+Grinder.jpg') }}" alt="商品画像">
-        </div>
-        <div class="flrama__content--item">
-            <p>コーヒーミル</p>
-        </div>
-    </div>
-    <div class="flrama__content">
-        <div class="flrama__content--img">
-            <img src="{{ asset('images/外出メイクアップセット.jpg') }}" alt="商品画像">
-        </div>
-        <div class="flrama__content--item">
-            <p>メイクセット</p>
-        </div>
-    </div>
-</div>
+@endsection
