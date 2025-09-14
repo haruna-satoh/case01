8ddfd99de49e2c3bb63f328f121527032b8471d3
haruna <news.sigeak@gmail.com>
2025-09-15 08:05:49 +0900

    purchasesテーブルを作成

---
+++ src/database/migrations/2025_09_12_230243_create_purchases_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePurchasesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('purchases', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('good_id');
+            $table->integer('quantity');//個数
+            $table->integer('price');//価格
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('purchases');
+    }
+}
