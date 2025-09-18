7ee801d7d3d28a65cc72c0580b33e8ab415f5047
haruna <news.sigeak@gmail.com>
2025-09-19 07:09:20 +0900

    検索機能を追加

--- src/app/Http/Controllers/ItemController.php
+++ src/app/Http/Controllers/ItemController.php
@@ -3,10 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Item;
 
 class ItemController extends Controller
 {
-    public function index() {
-        return view('index');
+    public function index(Request $request) {
+        $keyword = $request->input('keyword');
+
+        $query = Item::query();
+
+        if($keyword) {
+            $query->where('name', 'like', "%{$keyword}%");
+        }
+
+        $items = $query->get();
+
+        return view('index', compact('items', 'keyword'));
     }
 }
