326c4007b70d0440fbb9965c4930958e16b12db8
haruna <news.sigeak@gmail.com>
2025-09-20 05:22:12 +0900

    検索機能を実装

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -4,87 +4,23 @@
     <div class="flrama__flrama">
         <div class="flrama__title">
             <h3>おすすめ</h3>
-            <h3>マイリスト</h3>
+            <form action="">
+                @csrf
+                <button>マイリスト</button>
+            </form>
         </div>
-        <div class="flrama__content">
-            <div class="flrama__content--img">
-                <img src="{{ asset('images/Armani+Mens+Clock.jpg') }}" alt="商品画像">
-            </div>
-            <div class="flrama__content--item">
-                <p>腕時計</p>
-            </div>
-        </div>
-        <div class="flrama__content">
-            <div class="flrama__content--img">
-                <img src="{{ asset('images/HDD+Hard+Disk.jpg') }}" alt="商品画像">
-            </div>
-            <div class="flrama__content--item">
-                <p>HDD</p>
-            </div>
-        </div>
-        <div class="flrama__content">
-            <div class="flrama__content--img">
-                <img src="{{ asset('images/iLoveIMG+d.jpg') }}" alt="商品画像">
-            </div>
-            <div class="flrama__content--item">
-                <p>玉ねぎ3束</p>
-            </div>
-        </div>
-        <div class="flrama__content">
-            <div class="flrama__content--img">
-                <img src="{{ asset('images/Leather+Shoes+Product+Photo.jpg') }}" alt="商品画像">
-            </div>
-            <div class="flrama__content--item">
-                <p>革靴</p>
-            </div>
-        </div>
-        <div class="flrama__content">
-            <div class="flrama__content--img">
-                <img src="{{ asset('images/Living+Room+Laptop.jpg') }}" alt="商品画像">
-            </div>
-            <div class="flrama__content--item">
-                <p>ノートPC</p>
-            </div>
-        </div>
-        <div class="flrama__content">
-            <div class="flrama__content--img">
-                <img src="{{ asset('images/Music+Mic+4632231.jpg') }}" alt="商品画像">
-            </div>
-            <div class="flrama__content--item">
-                <p>マイク</p>
-            </div>
-        </div>
-        <div class="flrama__content">
-            <div class="flrama__content--img">
-                <img src="{{ asset('images/Purse+fashion+pocket.jpg') }}" alt="商品画像">
-            </div>
-            <div class="flrama__content--item">
-                <p>ショルダーバッグ</p>
-            </div>
-        </div>
-        <div class="flrama__content">
-            <div class="flrama__content--img">
-                <img src="{{ asset('images/Tumbler+souvenir.jpg') }}" alt="商品画像">
-            </div>
-            <div class="flrama__content--item">
-                <p>タンブラー</p>
-            </div>
-        </div>
-        <div class="flrama__content">
-            <div class="flrama__content--img">
-                <img src="{{ asset('images/Waitress+with+Coffee+Grinder.jpg') }}" alt="商品画像">
-            </div>
-            <div class="flrama__content--item">
-                <p>コーヒーミル</p>
-            </div>
-        </div>
-        <div class="flrama__content">
-            <div class="flrama__content--img">
-                <img src="{{ asset('images/外出メイクアップセット.jpg') }}" alt="商品画像">
-            </div>
-            <div class="flrama__content--item">
-                <p>メイクセット</p>
-            </div>
-        </div>
-    </div>
+        @if ($items->count() > 0)
+            @foreach ($items as $item)
+                <div class="flrama__content">
+                    <div class="flrama__content--img">
+                        <img src="{{ asset('images/' . $item->image) }}" alt="商品画像">
+                    </div>
+                    <div class="flrama__content--item">
+                        <p>{{  $item->name }}</p>
+                    </div>
+                </div>
+            @endforeach
+        @else
+            <p>該当する商品はありません。</p>
+        @endif
 @endsection
