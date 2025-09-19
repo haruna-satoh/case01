1758ddb6c0f67f521552cd22edc50474931f473c
haruna <news.sigeak@gmail.com>
2025-09-20 04:53:15 +0900

    usersテーブルにカラムを追加

--- src/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->string('icon')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->rememberToken();
             $table->timestamps();
         });
     }
