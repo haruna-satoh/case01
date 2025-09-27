b3d16c1fa2c40ec41898b629e8b68f97399671f8
haruna <news.sigeak@gmail.com>
2025-09-27 21:27:20 +0900

    ヘッダーの継承

---
+++ src/resources/views/items/index.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
+@endsection
+
+@section('content')
+    <div class="product-list">
+        <div class="product-list__title">
+            <a href="/" class="product-list__button {{ request()->query('tab') !== 'mylist' ? 'is-active' : '' }}">おすすめ</a>
+            <a href="/?tab=mylist" class="product-list__button {{ request()->query('tab') === 'mylist' ? 'is-active' : '' }}">マイリスト</a>
+        </div>
+        @if ($items->count() > 0)
+            <div class="product-list__content">
+                @foreach ($items as $item)
+                    <div class="product-list__content--item">
+                        <div class="product-list__content--img">
+                            <img src="{{ asset('images/' . $item->image) }}" alt="商品画像">
+                            @if ($item->purchase)
+                                <span class="sold-badge">Sold</span>
+                            @endif
+                        </div>
+                        <p>{{  $item->name }}</p>
+                    </div>
+                @endforeach
+            </div>
+        @else
+            <p>該当する商品はありません。</p>
+        @endif
+@endsection
---
+++ src/resources/views/items/sell.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('css')
+    
+@endsection
+
+@section('content')
+    
+@endsection
