e00f25316836d4953e52b4754dbe805e6340711f
haruna <news.sigeak@gmail.com>
2025-09-27 22:19:26 +0900

    商品画像

--- src/resources/views/items/sell.blade.php
+++ src/resources/views/items/sell.blade.php
@@ -9,5 +9,15 @@
         <div class="sell__title">
             <h2>商品の出品</h2>
         </div>
+        <form action="/sell" method=post class="form">
+            @csrf
+            <div class="form__img">
+                <p>商品画像</p>
+                <img src="{{ asset('images/') }}" alt="">
+                <button class="form__button">
+                    画像を選択する
+                </button>
+            </div>
+        </form>
     </div>
 @endsection
