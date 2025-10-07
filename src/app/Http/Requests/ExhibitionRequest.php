77a0e54b6591bf27ddabcd8c6934616001703ef5
haruna <news.sigeak@gmail.com>
2025-10-08 06:01:06 +0900

    エラーメッセージを編集

--- src/app/Http/Requests/ExhibitionRequest.php
+++ src/app/Http/Requests/ExhibitionRequest.php
@@ -27,9 +27,26 @@ class ExhibitionRequest extends FormRequest
             'name' => 'required',
             'explain' => 'required|max:255',
             'image' => 'required|mimes:jpeg,png',
-            'category' => 'required',
+            'options' => 'required|array',
+            'options.*' => 'exists:categories,id',
             'condition' => 'required',
             'price' => 'required|integer|min:0',
+        ];
+    }
+
+    public function messages ()
+    {
+        return [
+            'name.required' => '商品名を入力してください',
+            'explain.required' => '商品の説明を入力してください',
+            'explain.max' => '商品の説明を255文字以内で入力してください',
+            'image.required' => '商品の画像をアップロードしてください',
+            'image.mimes' => '拡張子が.jpegもしくは.pngのものを選択してください',
+            'options.required' => 'カテゴリーを選択してください',
+            'condition.required' => 'コンディションを選択してください',
+            'price.required' => '価格を入力してください',
+            'price.integer' => '価格を数値で入力してください',
+            'price.min' => '価格を0円以上で入力してください',
         ];
     }
 }
