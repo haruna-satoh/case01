cda031ab819a7bb22f0de54beee4a327e0763a49
haruna <news.sigeak@gmail.com>
2025-10-12 20:21:17 +0900

    クラス分け

--- src/resources/views/items/sell.blade.php
+++ src/resources/views/items/sell.blade.php
@@ -12,80 +12,84 @@
         <form action="{{ route('items.store') }}" method=post enctype="multipart/form-data" class="form">
             @csrf
             <div class="form__img">
-                <p>商品画像</p>
-                <img src="" alt="">
-                <label class="form__img--button">
-                    画像を選択する
-                    <input type="file" name="image" accept="image/jpeg,image/png">
-                </label>
-            </div>
-            <div class="from__error">
-                @error('image')
-                    {{ $message }}
-                @enderror
+                    <p>商品画像</p>
+                <div class="form__detail--img">
+                    <img src="" alt="">
+                    <label class="form__img--button">
+                        画像を選択する
+                        <input type="file" name="image" accept="image/jpeg,image/png">
+                    </label>
+                </div>
+                <div class="form__error">
+                    @error('image')
+                        {{ $message }}
+                    @enderror
+                </div>
             </div>
             <div class="form__detail">
                 <h3>商品の詳細</h3>
             </div>
-            <div class="from__detail--title">
+            <div class="form__detail--title">
                 <p>カテゴリー</p>
             </div>
             <div class="form__detail--content">
-                <div class="form__detail--button">
-                    <input type="checkbox" id="1" name="options[]" value="1">
-                    <label for="1">ファッション</label>
-                </div>
-                <div class="form__detail--button">
-                    <input type="checkbox" id="2" name="options[]" value="2">
-                    <label for="2">家電</label>
-                </div>
-                <div class="form__detail--button">
-                    <input type="checkbox" id="3" name="options[]" value="3">
-                    <label for="3">インテリア</label>
-                </div>
-                <div class="form__detail--button">
-                    <input type="checkbox" id="4" name="options[]" value="4">
-                    <label for="4">レディース</label>
-                </div>
-                <div class="form__detail--button">
-                    <input type="checkbox" id="5" name="options[]" value="5">
-                    <label for="5">メンズ</label>
-                </div>
-                <div class="form__detail--button">
-                    <input type="checkbox" id="6" name="options[]" value="6">
-                    <label for="6">コスメ</label>
-                </div>
-                <div class="form__detail--button">
-                    <input type="checkbox" id="7" name="options[]" value="7">
-                    <label for="7">本</label>
-                </div>
-                <div class="form__detail--button">
-                    <input type="checkbox" id="8" name="options[]" value="8">
-                    <label for="8">ゲーム</label>
-                </div>
-                <div class="form__detail--button">
-                    <input type="checkbox" id="9" name="options[]" value="9">
-                    <label for="9">スポーツ</label>
-                </div>
-                <div class="form__detail--button">
-                    <input type="checkbox" id="10" name="options[]" value="10">
-                    <label for="10">キッチン</label>
-                </div>
-                <div class="form__detail--button">
-                    <input type="checkbox" id="11" name="options[]" value="11">
-                    <label for="11">ハンドメイド</label>
-                </div>
-                <div class="form__detail--button">
-                    <input type="checkbox" id="12" name="options[]" value="12">
-                    <label for="12">アクセサリー</label>
-                </div>
-                <div class="form__detail--button">
-                    <input type="checkbox" id="13" name="options[]" value="13">
-                    <label for="13">おもちゃ</label>
-                </div>
-                <div class="form__detail--button">
-                    <input type="checkbox" id="14" name="options[]" value="14">
-                    <label for="14">ベビー・キッズ</label>
+                <div class="form__detail--buttons">
+                    <div class="form__detail--button">
+                        <input type="checkbox" id="1" name="options[]" value="1">
+                        <label for="1">ファッション</label>
+                    </div>
+                    <div class="form__detail--button">
+                        <input type="checkbox" id="2" name="options[]" value="2">
+                        <label for="2">家電</label>
+                    </div>
+                    <div class="form__detail--button">
+                        <input type="checkbox" id="3" name="options[]" value="3">
+                        <label for="3">インテリア</label>
+                    </div>
+                    <div class="form__detail--button">
+                        <input type="checkbox" id="4" name="options[]" value="4">
+                        <label for="4">レディース</label>
+                    </div>
+                    <div class="form__detail--button">
+                        <input type="checkbox" id="5" name="options[]" value="5">
+                        <label for="5">メンズ</label>
+                    </div>
+                    <div class="form__detail--button">
+                        <input type="checkbox" id="6" name="options[]" value="6">
+                        <label for="6">コスメ</label>
+                    </div>
+                    <div class="form__detail--button">
+                        <input type="checkbox" id="7" name="options[]" value="7">
+                        <label for="7">本</label>
+                    </div>
+                    <div class="form__detail--button">
+                        <input type="checkbox" id="8" name="options[]" value="8">
+                        <label for="8">ゲーム</label>
+                    </div>
+                    <div class="form__detail--button">
+                        <input type="checkbox" id="9" name="options[]" value="9">
+                        <label for="9">スポーツ</label>
+                    </div>
+                    <div class="form__detail--button">
+                        <input type="checkbox" id="10" name="options[]" value="10">
+                        <label for="10">キッチン</label>
+                    </div>
+                    <div class="form__detail--button">
+                        <input type="checkbox" id="11" name="options[]" value="11">
+                        <label for="11">ハンドメイド</label>
+                    </div>
+                    <div class="form__detail--button">
+                        <input type="checkbox" id="12" name="options[]" value="12">
+                        <label for="12">アクセサリー</label>
+                    </div>
+                    <div class="form__detail--button">
+                        <input type="checkbox" id="13" name="options[]" value="13">
+                        <label for="13">おもちゃ</label>
+                    </div>
+                    <div class="form__detail--button">
+                        <input type="checkbox" id="14" name="options[]" value="14">
+                        <label for="14">ベビー・キッズ</label>
+                    </div>
                 </div>
                 <div class="form__error">
                     @error('options')
