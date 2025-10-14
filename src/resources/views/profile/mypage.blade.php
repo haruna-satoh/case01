d3999a61f51f6af457f8828cfc8f0efedbdbf9e8
haruna <news.sigeak@gmail.com>
2025-10-14 20:39:18 +0900

    プロフィール編集ページに遷移設定

--- src/resources/views/profile/mypage.blade.php
+++ src/resources/views/profile/mypage.blade.php
@@ -5,5 +5,13 @@
 @endsection
 
 @section('content')
-    
+    <div class="icon__img">
+        <img src="" alt="">
+    </div>
+    <p class="user__name">
+        ユーザー名
+    </p>
+    <a href="/mypage/profile">
+        プロフィールを編集
+    </a>
 @endsection
