7c7f641c797412c013b5846e6fba18ee5467276f
haruna <news.sigeak@gmail.com>
2025-10-15 22:31:01 +0900

    出品商品と購入商品の表示

--- src/resources/views/profile/mypage.blade.php
+++ src/resources/views/profile/mypage.blade.php
@@ -7,13 +7,31 @@
 @section('content')
 <div class="mypage__user">
     <div class="user__icon--img">
-        <img src="" alt="">
+        <img src="{{ $user->icon ? asset('storage/'.$user->icon) : asset('images/default-icon.png') }}" alt="">
     </div>
     <p class="user__name">
-        ユーザー名
+        {{ $user->name }}
     </p>
     <a href="/mypage/profile" class="link__button">
         プロフィールを編集
     </a>
 </div>
-@endsection
+<div class="mypage-list">
+        <div class="mypage-list__title">
+            <a href="/" class="mypage-list__button {{ request()->query('tab') !== 'mylist' ? 'is-active' : '' }}">出品した商品</a>
+            <a href="/?tab=mylist" class="mypage-list__button {{ request()->query('tab') === 'mylist' ? 'is-active' : '' }}">購入した商品</a>
+        </div>
+        @if ($items->count() > 0)
+            <div class="mypage-list__content">
+                @foreach ($items as $item)
+                    <div class="mypage-list__content--item">
+                        <div class="mypage-list__content--img">
+                            <img src="{{ asset('images/' . $item->image) }}" alt="商品画像">
+                        </div>
+                        <p>{{  $item->name }}</p>
+                    </div>
+                @endforeach
+            </div>
+        @endif
+    @endsection
+</div>
