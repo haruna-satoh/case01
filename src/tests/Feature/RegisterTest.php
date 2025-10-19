de916e2a52d011ffaf27e9c44a1ba5c77da115f0
haruna <news.sigeak@gmail.com>
2025-10-19 23:18:31 +0900

    名前み入力時のバリデーションテスト

---
+++ src/tests/Feature/RegisterTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RegisterTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function 名前が未入力の場合はバリデーションエラーになる() {
+        $formData = [
+            'name' => '',
+            'email' => 'test@example.com',
+            'password' => 'password123',
+            'password_confirmation' => 'password123',
+        ];
+
+        $response = $this->post('/register', $formData);
+
+        $response->assertSessionHasErrors(['name']);
+
+        $this->assertStringContainsString(
+            'お名前を入力してください',
+            session('errors')->first('name')
+        );
+    }
+}
