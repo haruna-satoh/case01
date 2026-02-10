23e4d56f2928db6a6d9dbc447594ce07688338b6
haruna <news.sigeak@gmail.com>
2026-02-10 17:18:50 +0900

    commentsテーブルとのリレーション設定

--- src/app/Models/Item.php
+++ src/app/Models/Item.php
@@ -37,4 +37,8 @@ class Item extends Model
     public function user() {
         return $this->belongsTo(User::class);
     }
+
+    public function comments() {
+        return $this->hasMany(Comment::class);
+    }
 }
