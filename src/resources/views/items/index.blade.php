14960d86eb424f99d4e0b7eed08e7bfa56099152
haruna <news.sigeak@gmail.com>
2026-02-13 11:14:17 +0900

    購入済商品の表示設定

--- src/resources/views/items/index.blade.php
+++ src/resources/views/items/index.blade.php
@@ -17,7 +17,7 @@
                         <a href="{{ route('item.show', $item->id) }}" class="product-link">
                             <div class="product-list__content--img">
                                 <img src="{{ asset('images/' . $item->image) }}" alt="商品画像">
-                                @if ($item->purchase)
+                                @if ($item->purchases_count > 0)
                                     <span class="sold-badge">Sold</span>
                                 @endif
                             </div>
